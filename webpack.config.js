f2e30c9e647f4d99467a2e95eb9d71693882338a
Neil Edwards <neil@plasticwater.com>
2016-07-10 22:17:12 -0400

    update some packages

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var webpack = require('webpack');
+
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  entry: [
+    path.resolve(ROOT_PATH, 'src/app/src/index'),
+  ],
+  resolveLoader: {
+      root: path.resolve(__dirname, 'node_modules')
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel'],
+    }]
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'src/app/build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(ROOT_PATH, 'src/app/build'),
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Lemon Vote'
+    })
+  ]
+};
