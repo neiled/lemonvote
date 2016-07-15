dfb65d4140d130aa9dc0c8d6f1752c65b25211f8
Neil Edwards <neil@plasticwater.com>
2016-07-15 15:42:23 +0000

    added router redux

--- src/app/src/index.jsx
+++ src/app/src/index.jsx
@@ -1,12 +1,33 @@
-import React from 'react';  
-import ReactDOM from 'react-dom';  
-import App from './components/App.jsx';  
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
+//import reducers from './reducers'
+
+import App from './components/App.jsx';  
 import styles from './styles/base.css';
 
-main();  
-function main() {  
-    const app = document.createElement('div');    
-    document.body.appendChild(app);    
-    ReactDOM.render(<App />, app); 
-}
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    //...reducers,
+    routing: routerReducer
+  })
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    { /* Tell the Router to use our enhanced history */ }
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
